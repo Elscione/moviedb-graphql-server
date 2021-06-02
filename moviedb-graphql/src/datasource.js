1fd6001dcc5b9b5295fc67eda2c520b46aaddb28
Yusuf Hendrawan <yusufhendrawan06@gmail.com>
2021-06-02 23:34:06 +0700

    Add page as parameter.

--- moviedb-graphql/src/datasource.js
+++ moviedb-graphql/src/datasource.js
@@ -1,16 +1,19 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
 class MovieDBAPI extends RESTDataSource {
+  #page = 1;
   constructor() {
     super();
     this.baseURL = 'https://api.themoviedb.org/3/';
   }
 
   willSendRequest(request) {
+    request.params.set('page', this.page);
     request.params.set('api_key', '5d87429e4f4f5989cd5b90ca3ff1c320');
   }
 
-  async discoverMovies() {
+  async discoverMovies(page) {
+    this.page = page;
     return this.get('discover/movie');
   }
 }
